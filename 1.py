099d59cfb4a539aad72fca45d33daf78f93ff71a
“koi154” <zer123456@naver.com>
2024-11-30 13:51:04 +0900

    11.30 수업

---
+++ 1.py
@@ -0,0 +1,45 @@
+print("hello world")
+print("hello", "Python")
+
+print("hello", "Python", 1, 2, "asdfe", sep=" ")
+
+
+ive = "안녕하세요!"
+print(ive)
+print(f"나는 {ive}입니다.")
+print("나는 ", ive, "입니다.", sep="")
+print("나는 " + ive + "입니다.")
+
+print(type(77))
+print(type(7.2))
+print(type("iv\"e"))
+print(type('iv\'e'))
+
+a = 77
+print (type(a))
+a = 7.2
+print (type(a))
+a = "ive"
+print (type(a))
+print ("111\t111")
+print ("111\n111")
+print ('111"111')
+print ("111'111")
+
+num = 10
+b_num = 0b1010
+h_num = 0xA
+
+print (num)
+print (b_num)
+print (h_num)
+
+print(bin(10))
+print(hex(10))
+
+#아스키 코드값과 문자
+print(ord('0'))
+print(ord('A'))
+print(chr(48))
+print(chr(65))
+
